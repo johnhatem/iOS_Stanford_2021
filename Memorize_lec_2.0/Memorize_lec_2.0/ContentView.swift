0597934130438e4814d6168a4dc3699d29d3299d
John Hatem <johnhatem@icloud.com>
2021-06-04 12:32:30 +0300

    Lectures 1 and 2

---
+++ Memorize_lec_2.0/Memorize_lec_2.0/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  Memorize_lec_2.0
+//
+//  Created by John Hatem on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var emojis: [String] = ["🚂", "🚀", "✈️", "🚁", "🚗", "🚌", "🚜", "🛶", "🚔", "🚑",
+                            "🚚", "🚲", "🛺", "🚈", "🚢", "🚤", "⛵️", "🚒", "🏎", "🚟"]
+    
+    @State var emojiCount: Int = 20
+
+    var body: some View {
+        VStack{
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+
+        
+    }
+    
+    var remove: some View {
+        Button { //Compact way of writing a Button
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+           Image(systemName: "plus.circle")
+        })
+    }
+    
+}
+
+
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            
+            ContentView()
+                .preferredColorScheme(.dark)
+
+        }
+    }
+}
---
+++ Memorize_lec_2.0/Memorize_lec_2.0/Memorize_lec_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_lec_2_0App.swift
+//  Memorize_lec_2.0
+//
+//  Created by John Hatem on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_lec_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
